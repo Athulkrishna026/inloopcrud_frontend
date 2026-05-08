bc61be61cb1a6c9ae2176f938a1d3226f48c374d
Athulkrishna026 <athuls026@gmail.com>
2026-05-08 11:57:30 +0530

    third commit

--- src/api/employeeApi.js
+++ src/api/employeeApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:5000/api/employees",
-  baseURL: "https://inloopcrud-backend.onrender.com/api/employees",
+  baseURL: "https://inloopcrud-backend.onrender.com",
 });
 
 export default API;
